20ed573c57e24705f56fc2ed2019c7c9cff0a4ad
savyasachi <imsavyasachi@gmail.com>
2022-07-21 19:56:06 +0530

    is it works

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -18,7 +18,7 @@ function Products({ productInfo, goToProductDetailsPage }) {
         style={{ textAlign: "left" }}
       >
         <Link
-          to={`myshope/products/${productInfo.id}`}
+          to={`/products/${productInfo.id}`}
           className="card-title"
           style={{ textDecoration: "none" }}
         >
@@ -36,7 +36,7 @@ function Products({ productInfo, goToProductDetailsPage }) {
         <hr />
         <div className=" mt-auto">
           <Link
-            to={`myshope/products/${productInfo.id}`}
+            to={`/products/${productInfo.id}`}
             className="btn "
             onClick={() => addToCart()}
             style={{ backgroundColor: "pink" }}
